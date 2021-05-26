614ff72196f3b02ba2b2dd32a6bdeb43b5c00801
sammightyt <64436585+sammightyt@users.noreply.github.com>
2021-05-26 11:27:01 -0500

    Update index.js
    cvfhytj

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ document.getElementById("bad").onclick = function(){
   document.getElementById("id").src = `https://cdn.vox-cdn.com/thumbor/rnmeN-viwzdK3Vlggs-O65XjURk=/0x0:3000x2386/1200x800/filters:focal(1260x953:1740x1433)/cdn.vox-cdn.com/uploads/chorus_image/image/59456387/AP_740424045.0.jpg`;
   $("iframe").show(2000,function(){
     $("#hello").html("<h1>No, You have brought DESTRUCTION</h1>")
-  }).animate({"height":"300px","width":"500px"})
+  }).animate({"height":"300px","width":"5000px"})
 }
 
 
